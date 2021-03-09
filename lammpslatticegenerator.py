2aa298f1e56bf9bc16b17eda0ed47827944b986d
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2021-03-09 23:03:17 +0000

    Extract more to functions

--- lammpslatticegenerator.py
+++ lammpslatticegenerator.py
@@ -1,32 +1,26 @@
 import argparse
+from typing import List, TextIO
 
 
 def main(args: dict):
 
-    if args["output_type"] == "lammps":
-        output_file = open("out.lmp", "w")
-    else:
-        output_file = open("out.xyz", "w")
-
     if args["type"] == "al2cu":
-        unitcelldimensions = [6.067, 6.067, 4.877]
+        unit_cell_dimensions = [6.067, 6.067, 4.877]
         num_particles = 12 * args["lattice_size"] ** 3
     else:
         # cubic lattice
-        unitcelldimensions = [1, 1, 1]
+        unit_cell_dimensions = [1, 1, 1]
         num_particles = args["lattice_size"] ** 3
 
     volume = num_particles / args["density"]
-    sidelenratio = unitcelldimensions[0] / unitcelldimensions[2]
-    sidez = (volume / (sidelenratio * sidelenratio)) ** (1.0 / 3)
-    sidexy = sidez * sidelenratio
-    unitcellsidexy = sidexy / args["lattice_size"]
-    unitcellsidez = sidez / args["lattice_size"]
+    side_len_ratio = unit_cell_dimensions[0] / unit_cell_dimensions[2]
+    side_z = (volume / (side_len_ratio * side_len_ratio)) ** (1.0 / 3)
+    side_xy = side_z * side_len_ratio
+    unit_cell_side_xy = side_xy / args["lattice_size"]
+    unit_cell_side_z = side_z / args["lattice_size"]
 
-    print("unit cell side x + y = " + str(unitcellsidexy))
-    print("unit cell side z = " + str(unitcellsidez))
-
-    particlecounter = 1
+    print(f"unit cell side x + y = {unit_cell_side_xy}")
+    print(f"unit cell side z = {unit_cell_side_z}")
 
     al2cucoordarray = [[0.65810, 0.15810, 0.50000],
                        [0.15810, 0.65810, 0.00000],
@@ -40,56 +34,85 @@ def main(args: dict):
                        [0.00000, 0.00000, 0.25000],
                        [0.50000, 0.50000, 0.25000],
                        [0.00000, 0.00000, 0.75000]]
-
-    if args["output_type"] == "xyz":
-        output_file.write(str(num_particles) + "\n")
-
-    output_file.write("Auto generated 11A crystal lattice for LAMMPS\n")
-
     if args["output_type"] == "lammps":
-        output_file.write(f"\n{num_particles} atoms\n\n")
-        output_file.write("2 atom types\n\n")
-        output_file.write(f"{-0.5 * sidexy:.7f} {sidexy * 0.5:.7f} xlo xhi\n")
-        output_file.write(f"{-0.5 * sidexy:.7f} {sidexy * 0.5:.7f} ylo yhi\n")
-        output_file.write(f"{-0.5 * sidez:.7f} {sidez * 0.5:.7f} zlo zhi\n\n")
-        output_file.write("Masses\n\n1 1\n2 1\n\nAtoms\n\n")
+        file_name = "out.lmp"
+    else:
+        file_name = "out.xyz"
 
-    if args["type"] == "al2cu":
-        for x in range(args["lattice_size"]):
-            for y in range(args["lattice_size"]):
-                for z in range(args["lattice_size"]):
-                    for unit_cell_counter in range(12):
-                        lammpsprefix = f"{particlecounter}\t"
-                        if unit_cell_counter > 7:
-                            particle_type = 2
-                        else:
-                            particle_type = 1
-                        x_pos = al2cucoordarray[unit_cell_counter][0] * unitcellsidexy + x * unitcellsidexy - 0.5 * sidexy
-                        y_pos = al2cucoordarray[unit_cell_counter][1] * unitcellsidexy + y * unitcellsidexy - 0.5 * sidexy
-                        z_pos = al2cucoordarray[unit_cell_counter][2] * unitcellsidez + z* unitcellsidez - 0.5 * sidez
-                        if args["output_type"] == "lammps":
-                            output_file.write(lammpsprefix)
-                        output_file.write(f"{particle_type}\t{x_pos:.7f}\t{y_pos:.7f}\t{z_pos:.7f}\n")
-                        particlecounter += 1
-    else:   # Cubic lattice
-        for x in range(args["lattice_size"]):
-            for y in range(args["lattice_size"]):
-                for z in range(args["lattice_size"]):
-                    if particlecounter % (args["ratio"] + 1) == 0:
+    with open(file_name, 'w') as output_file:
+        write_header(args["output_type"], num_particles, output_file, side_xy, side_z)
+
+        if args["type"] == "al2cu":
+            write_al2cu(al2cucoordarray, args, output_file, side_xy, side_z, unit_cell_side_xy,
+                        unit_cell_side_z)
+        else:   # Cubic lattice
+            write_cubic(args, output_file, side_xy, side_z, unit_cell_side_xy, unit_cell_side_z)
+
+
+def write_cubic(args: dict, output_file: TextIO, side_xy: float, side_z: float,
+                cell_side_xy: float, cell_side_z: float):
+    particle_counter = 1
+    for x in range(args["lattice_size"]):
+        for y in range(args["lattice_size"]):
+            for z in range(args["lattice_size"]):
+                if args["output_type"] == "lammps":
+                    output_file.write(f"{particle_counter}\t")
+
+                if particle_counter % (args["ratio"] + 1) == 0:
+                    particle_type = 2
+                else:
+                    particle_type = 1
+
+                x_pos = x * cell_side_xy - 0.5 * side_xy
+                y_pos = y * cell_side_xy - 0.5 * side_xy
+                z_pos = z * cell_side_z - 0.5 * side_z
+                output_file.write(f"{particle_type}\t{x_pos:.7f}\t{y_pos:.7f}\t{z_pos:.7f}\n")
+                particle_counter += 1
+
+
+def write_al2cu(al2cu_coords: List[List[int]], args: dict, output_file: TextIO, side_xy: float,
+                side_z: float, unitcellside_xy: float, unitcellside_z: float):
+    particle_counter = 1
+    for x in range(args["lattice_size"]):
+        for y in range(args["lattice_size"]):
+            for z in range(args["lattice_size"]):
+                for unit_cell_counter in range(12):
+                    if args["output_type"] == "lammps":
+                        output_file.write(f"{particle_counter}\t")
+
+                    if unit_cell_counter > 7:
                         particle_type = 2
                     else:
                         particle_type = 1
-                    x_pos = x * unitcellsidexy - 0.5 * sidexy
-                    y_pos = y * unitcellsidexy - 0.5 * sidexy
-                    z_pos = z * unitcellsidez - 0.5 * sidez
-                    lammpsprefix = f"{particlecounter}\t"
-                    lineout = f"{particle_type}\t{x_pos:.7f}\t{y_pos:.7f}\t{z_pos:.7f}\n"
-                    if args["output_type"] == "lammps":
-                        output_file.write(lammpsprefix + lineout)
-                    else:
-                        output_file.write(lineout)
-                    particlecounter += 1
-    output_file.close()
+
+                    x_pos = al2cu_coords[unit_cell_counter][0] * unitcellside_xy + x * unitcellside_xy - 0.5 * side_xy
+                    y_pos = al2cu_coords[unit_cell_counter][1] * unitcellside_xy + y * unitcellside_xy - 0.5 * side_xy
+                    z_pos = al2cu_coords[unit_cell_counter][2] * unitcellside_z + z * unitcellside_z - 0.5 * side_z
+                    output_file.write(f"{particle_type}\t{x_pos:.7f}\t{y_pos:.7f}\t{z_pos:.7f}\n")
+                    particle_counter += 1
+
+
+def write_header(output_type: str, num_particles: int, output_file: TextIO, side_xy: float,
+                 side_z: float):
+    if output_type == "xyz":
+        write_xyz_header(num_particles, output_file)
+    if output_type == "lammps":
+        write_lammps_header(num_particles, output_file, side_xy, side_z)
+
+
+def write_lammps_header(num_particles: int, output_file: TextIO, side_xy: float, side_z: float):
+    output_file.write(f"\n{num_particles} atoms\n\n")
+    output_file.write("2 atom types\n\n")
+    output_file.write(f"{-0.5 * side_xy:.7f} {side_xy * 0.5:.7f} xlo xhi\n")
+    output_file.write(f"{-0.5 * side_xy:.7f} {side_xy * 0.5:.7f} ylo yhi\n")
+    output_file.write(f"{-0.5 * side_z:.7f} {side_z * 0.5:.7f} zlo zhi\n\n")
+    output_file.write("Masses\n\n1 1\n2 1\n\nAtoms\n\n")
+    output_file.write("Auto generated crystal lattice for LAMMPS\n")
+
+
+def write_xyz_header(num_particles: int, output_file: TextIO):
+    output_file.write(f"{num_particles}\n")
+    output_file.write("Auto generated crystal lattice\n")
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,1 +1,18 @@
 # Generate crystal lattices in LAMMPS or XYZ format
+
+Writes either cubic or Al<sub>2</sub>Cu lattices to either an [xyz file](https://en.wikipedia.org/wiki/XYZ_file_format)
+ or a [lammps data file](https://lammps.sandia.gov/doc/read_data.html#format-of-a-data-file).
+
+Command line arguments:
+-o: The format of the output, 'xyz' or 'lammps', default is 'xyz'
+-s: The integer number of unit cells in each axis, default is 5
+-d: The number density of the lattice, default = 1
+-t: The lattice type, 'cubic' or 'al2cu', default is"al2cu"
+-r: Ratio of A to B particles. This argument is ignored for Al2Cu lattices, default = 4
+
+## Citation
+
+If you use this code in work that leads to a publication please cite this code: 
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.4592537.svg)](https://doi.org/10.5281/zenodo.4592537)
+
+[Software is a valuable research output!](https://www.software.ac.uk/about/manifesto) 
